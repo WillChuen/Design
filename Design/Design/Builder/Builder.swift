e17b9366df18da36436b2eafaca349c1950cc395
zhongwei <1063163323@qq.com>
2022-03-22 16:01:28 +0800

    添加抽象工厂和生成器模式代码

---
+++ Design/Design/Builder/Builder.swift
@@ -0,0 +1,12 @@
+//
+//  Builder.swift
+//  Design
+//
+//  Created by 钟维 on 2022/3/22.
+//
+
+import UIKit
+
+class Builder: NSObject {
+
+}
---
+++ Design/Design/Factory/AbstractFactory.swift
@@ -0,0 +1,12 @@
+//
+//  AbstractFactory.swift
+//  Design
+//
+//  Created by 钟维 on 2022/3/22.
+//
+
+import UIKit
+
+class AbstractFactory: NSObject {
+
+}
