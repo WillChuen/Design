991c017bc042d1fc3753f0860abe843baa19e338
zhongwei <1063163323@qq.com>
2022-03-23 01:46:48 +0800

    责任链 命令模式 测试用例

--- Design/Design/Chain/Chain.swift
+++ Design/Design/Chain/Chain.swift
@@ -6,42 +6,46 @@
 //
 
 import Foundation
+import XCTest
 
+/*
+ 责任链模式是一种行为设计模式， 允许你将请求沿着处理者链进行发送。 收到请求后， 每个处理者均可对请求进行处理， 或将其传递给链上的下个处理者。
+ 责任链模式结构
+1 处理者 （Handler） 声明了所有具体处理者的通用接口。 该接口通常仅包含单个方法用于请求处理， 但有时其还会包含一个设置链上下个处理者的方法。
+2 基础处理者 （Base Handler） 是一个可选的类， 你可以将所有处理者共用的样本代码放置在其中。通常情况下， 该类中定义了一个保存对于下个处理者引用的成员变量。 客户端可通过将处理者传递给上个处理者的构造函数或设定方法来创建链。 该类还可以实现默认的处理行为： 确定下个处理者存在后再将请求传递给它。
+3 具体处理者 （Concrete Handlers） 包含处理请求的实际代码。 每个处理者接收到请求后， 都必须决定是否进行处理， 以及是否沿着链传递请求。处理者通常是独立且不可变的， 需要通过构造函数一次性地获得所有必要地数据。
+4 客户端 （Client） 可根据程序逻辑一次性或者动态地生成链。 值得注意的是， 请求可发送给链上的任意一个处理者， 而非必须是第一个处理者。
+ 
+ 责任链模式在 Swift 程序中并不常见， 因为它仅在代码与对象链打交道时才能发挥作用。
+ 
+ */
 
-/// The Handler interface declares a method for building the chain of handlers.
-/// It also declares a method for executing a request.
+// 处理者 （Handler） 声明了所有具体处理者的通用接口。 该接口通常仅包含单个方法用于请求处理， 但有时其还会包含一个设置链上下个处理者的方法。
 protocol Handler: AnyObject {
-
+    
     @discardableResult
     func setNext(handler: Handler) -> Handler
-
+    
     func handle(request: String) -> String?
-
+    
     var nextHandler: Handler? { get set }
 }
 
 extension Handler {
-
+    
     func setNext(handler: Handler) -> Handler {
         self.nextHandler = handler
-
-        /// Returning a handler from here will let us link handlers in a
-        /// convenient way like this:
-        /// monkey.setNext(handler: squirrel).setNext(handler: dog)
         return handler
     }
-
     func handle(request: String) -> String? {
         return nextHandler?.handle(request: request)
     }
 }
 
-/// All Concrete Handlers either handle a request or pass it to the next handler
-/// in the chain.
+///具体处理者 （Concrete Handlers） 包含处理请求的实际代码。 每个处理者接收到请求后， 都必须决定是否进行处理， 以及是否沿着链传递请求。处理者通常是独立且不可变的， 需要通过构造函数一次性地获得所有必要地数据。
 class MonkeyHandler: Handler {
-
+    
     var nextHandler: Handler?
-
     func handle(request: String) -> String? {
         if (request == "Banana") {
             return "Monkey: I'll eat the " + request + ".\n"
@@ -51,10 +55,10 @@ class MonkeyHandler: Handler {
     }
 }
 
+///具体处理者 （Concrete Handlers） 包含处理请求的实际代码。 每个处理者接收到请求后， 都必须决定是否进行处理， 以及是否沿着链传递请求。处理者通常是独立且不可变的， 需要通过构造函数一次性地获得所有必要地数据。
 class SquirrelHandler: Handler {
-
+    
     var nextHandler: Handler?
-
     func handle(request: String) -> String? {
 
         if (request == "Nut") {
@@ -65,6 +69,7 @@ class SquirrelHandler: Handler {
     }
 }
 
+///具体处理者 （Concrete Handlers） 包含处理请求的实际代码。 每个处理者接收到请求后， 都必须决定是否进行处理， 以及是否沿着链传递请求。处理者通常是独立且不可变的， 需要通过构造函数一次性地获得所有必要地数据。
 class DogHandler: Handler {
 
     var nextHandler: Handler?
@@ -85,18 +90,33 @@ class ChainClient {
     static func someClientCode(handler: Handler) {
 
         let food = ["Nut", "Banana", "Cup of coffee"]
-
         for item in food {
-
             print("Client: Who wants a " + item + "?\n")
-
             guard let result = handler.handle(request: item) else {
                 print("  " + item + " was left untouched.\n")
                 return
             }
-
             print("  " + result)
         }
     }
     // ...
 }
+
+/// Let's see how it all works together.
+class ChainOfResponsibilityConceptual: XCTestCase {
+ 
+    func test() {
+
+        let monkey = MonkeyHandler()
+        let squirrel = SquirrelHandler()
+        let dog = DogHandler()
+        monkey.setNext(handler: squirrel).setNext(handler: dog)
+
+        print("Chain: Monkey > Squirrel > Dog\n\n")
+        ChainClient.someClientCode(handler: monkey)
+        print()
+        print("Subchain: Squirrel > Dog\n\n")
+        ChainClient.someClientCode(handler: squirrel)
+    }
+}
+
--- Design/Design/Command/Command.swift
+++ Design/Design/Command/Command.swift
@@ -6,12 +6,26 @@
 //
 
 import Foundation
+import XCTest
 
+/*
+ 命令模式是一种行为设计模式， 它可将请求转换为一个包含与请求相关的所有信息的独立对象。 该转换让你能根据不同的请求将方法参数化、 延迟请求执行或将其放入队列中， 且能实现可撤销操作。
+ 命令模式结构
+1 发送者 （Sender）——亦称 “触发者 （Invoker）”——类负责对请求进行初始化， 其中必须包含一个成员变量来存储对于命令对象的引用。 发送者触发命令， 而不向接收者直接发送请求。 注意， 发送者并不负责创建命令对象： 它通常会通过构造函数从客户端处获得预先生成的命令。
+2 命令 （Command） 接口通常仅声明一个执行命令的方法。
+3 具体命令 （Concrete Commands） 会实现各种类型的请求。 具体命令自身并不完成工作， 而是会将调用委派给一个业务逻辑对象。 但为了简化代码， 这些类可以进行合并。接收对象执行方法所需的参数可以声明为具体命令的成员变量。 你可以将命令对象设为不可变， 仅允许通过构造函数对这些成员变量进行初始化。
+4 接收者 （Receiver） 类包含部分业务逻辑。 几乎任何对象都可以作为接收者。 绝大部分命令只处理如何将请求传递到接收者的细节， 接收者自己会完成实际的工作。
+5 客户端 （Client） 会创建并配置具体命令对象。 客户端必须将包括接收者实体在内的所有请求参数传递给命令的构造函数。 此后， 生成的命令就可以与一个或多个发送者相关联了。
+ 
+ 命令模式在 Swift 代码中很常见。 大部分情况下， 它被用于代替包含行为的参数化 UI 元素的回调函数， 此外还被用于对任务进行排序和记录操作历史记录等
+ */
 
+///命令 （Command） 接口通常仅声明一个执行命令的方法。
 protocol Command {
     func execute()
 }
 
+///具体命令 （Concrete Commands） 会实现各种类型的请求。 具体命令自身并不完成工作， 而是会将调用委派给一个业务逻辑对象。 但为了简化代码， 这些类可以进行合并。接收对象执行方法所需的参数可以声明为具体命令的成员变量。你可以将命令对象设为不可变， 仅允许通过构造函数对这些成员变量进行初始化。
 class SimpleCommand: Command {
 
     private var payload: String
@@ -23,8 +37,10 @@ class SimpleCommand: Command {
     }
 }
 
+///具体命令 （Concrete Commands） 会实现各种类型的请求。 具体命令自身并不完成工作， 而是会将调用委派给一个业务逻辑对象。 但为了简化代码， 这些类可以进行合并。接收对象执行方法所需的参数可以声明为具体命令的成员变量。你可以将命令对象设为不可变， 仅允许通过构造函数对这些成员变量进行初始化。
 class ComplexCommand: Command {
-
+    
+    ///绝大部分命令只处理如何将请求传递到接收者的细节， 接收者自己会完成实际的工作。
     private var receiver: Receiver
     private var a: String
     private var b: String
@@ -42,17 +58,18 @@ class ComplexCommand: Command {
     }
 }
 
+///（Receiver） 类包含部分业务逻辑。 几乎任何对象都可以作为接收者。 绝大部分命令只处理如何将请求传递到接收者的细节， 接收者自己会完成实际的工作。
 class Receiver {
 
     func doSomething(_ a: String) {
         print("Receiver: Working on (" + a + ")\n")
     }
-
     func doSomethingElse(_ b: String) {
         print("Receiver: Also working on (" + b + ")\n")
     }
 }
 
+///发送者 （Sender）——亦称 “触发者 （Invoker）”——类负责对请求进行初始化， 其中必须包含一个成员变量来存储对于命令对象的引用。 发送者触发命令， 而不向接收者直接发送请求。 注意， 发送者并不负责创建命令对象： 它通常会通过构造函数从客户端处获得预先生成的命令。
 class Invoker {
 
     private var onStart: Command?
@@ -81,5 +98,18 @@ class Invoker {
         print("Invoker: Does anybody want something done after I finish?")
 
         onFinish?.execute()
+    }
+}
+
+class CommandConceptual: XCTestCase {
+
+    func test() {
+        
+        let invoker = Invoker()
+        invoker.setOnStart(SimpleCommand("Say Hi!"))
+
+        let receiver = Receiver()
+        invoker.setOnFinish(ComplexCommand(receiver, "Send email", "Save report"))
+        invoker.doSomethingImportant()
     }
 }
